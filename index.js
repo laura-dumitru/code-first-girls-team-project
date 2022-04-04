d5cbbce9a8d0699e53d9b4afb82ac3db307b5a38
Laura Dumitru <lauradumitru88@icloud.com>
2022-04-04 13:27:52 +0100

    Updated quote text

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const quotes = [
-  "“Wherever you go, there you are.” — Jon Kabat-Zinn",
+  "“Wherever you are, be there totally.” — Eckart Tolle",
   "“Live the actual moment. Only this actual moment is life.” — Thich Nhât Hanh",
   "“Nothing can harm you as much as your own thoughts unguarded.” — Buddha",
   "“Mindfulness isn’t difficult, we just need to remember to do it.” — Sharon Salzberg",
@@ -7,7 +7,8 @@ const quotes = [
   "“Meditation is not evasion; it is a serene encounter with reality.” — Thich Nhât Hanh",
   "“Open the window of your mind. Allow the fresh air, new lights and new truths to enter.” — Amit Ray",
   "“The mind is just like a muscle – the more you exercise it, the stronger it gets and the more it can expand.” — Idowu Koyenikan",
-  "“Life is a dance, Mindfulness is witnessing that dance.” — Amit Ray",
+  "“Life is a dance, mindfulness is witnessing that dance.” — Amit Ray",
+  "“Training your mind to be in the present moment is the number one key to making healthier choices.” — Susan Albers",
 ];
 
 const quoteDisplay = document.getElementById("quote");
